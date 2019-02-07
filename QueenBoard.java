52bb18425a0ad6625769acaecd1ec5a2b3feae8a
dramsammy <dramsammy@stuy.edu>
2019-02-07 09:33:52 -0500

    initialized board array

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,3 +1,12 @@
 public class QueenBoard{
   private int[][] board;
+  public QueenBoard(int size){
+    board = new int[size][size];
+    for (int i = 0; i < board[0].length; i++){
+      for (int a = 0; a < board.length; a++){
+        board[i][a] = 0;
+      }
+    }
+    
+  }
 }
