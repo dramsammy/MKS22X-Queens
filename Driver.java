abf420d0b1793f689faf5fc8052f152359b0525e
dramsammy <dramsammy@stuy.edu>
2019-02-11 23:48:23 -0500

    solve works i think?

--- Driver.java
+++ Driver.java
@@ -15,5 +15,9 @@ public class Driver{
     test.removeQueen(3,2);
     System.out.println(test);
     System.out.println(test.toStringDebug());
+    QueenBoard test1 = new QueenBoard(4);
+    System.out.println(test1.solve());
+    QueenBoard test2 = new QueenBoard(3);
+    System.out.println(test2.solve());
   }
 }
